6c632262f44603a12d6a38647d92371b11ab2648
absalon <luisopazo@gmail.com>
2018-08-25 22:34:39 -0300

    eclipse photon

--- kafka/kafka-consumer-producer/src/main/java/aopazo_kafka/test/App.java
+++ kafka/kafka-consumer-producer/src/main/java/aopazo_kafka/test/App.java
@@ -7,41 +7,85 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Properties;
-import java.util.Random;
+import org.apache.log4j.*;
 
 public class App {
 
-   public static void main(String[] args){
+	static Logger logger = Logger.getLogger("org.apache.kafka");
+	
+   public static void main(String[] args) throws UnknownHostException{
 
        Properties props = new Properties();
-       props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9092");
-       props.put(ProducerConfig.ACKS_CONFIG, "all");
-       props.put(ProducerConfig.RETRIES_CONFIG, 0);
-       props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-       props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+       props.put("client.id", InetAddress.getLocalHost().getHostName()); 
+       props.put("bootstrap.servers", args[0]);
+       
+       /**
+        * If acks=all, the producer will receive a success response from the broker once all in-sync
+        *  replicas received the message. This is the safest mode since you can make sure more than 
+        *  one broker has the message and that the message will survive even in the case of crash 
+        *  (more information on this in Chapter 5). However, the latency we discussed in the acks=1 
+        *  case will be even higher, since we will be waiting for more than just one broker to receive the message.
+        */
+       props.put("acks", "all");
+       /**
+        * retries parameter will control how many times the producer will retry sending the message before giving 
+        * up and notifying the client of an issue. By default, the producer will wait 100ms between retries, 
+        * but you can control this using the "retry.backoff.ms" parameter. 
+        */
+       props.put("retries", 10);
+       props.put("retry.backoff.ms", 300);
+       //props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,Integer.toString(Integer.MAX_VALUE)); // increase to infinity from default of 300 s
+
+       props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+       props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
        Producer<String, String> producer = new KafkaProducer<String, String>(props);
        TestCallback callback = new TestCallback();
-       Random rnd = new Random();
+
        for (long i = 0; i < 100 ; i++) {
            ProducerRecord<String, String> data = new ProducerRecord<String, String>(
-                   "test-topic", "key-" + i, "message-"+i );
+                   "test-topic", 
+                   "key-" + i +"-"+System.currentTimeMillis(), 
+                   "message-"+i 
+                  );
+           // ASYNC
            producer.send(data, callback);
+           /*
+            *  // SYNC
+            * try {
+        	    	producer.send(data).get();
+	        	} catch (Exception e) {
+	        	    logger.error(e);
+	        	} 
+            */
+           
        }
 
        producer.close();
    }
 
-
+   // if you want to invoke some code after the write has completed you can also provide a callback. 
+   // In Java this is implemented as a Callback object:
    private static class TestCallback implements Callback {
        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
            if (e != null) {
-               System.out.println("Error while producing message to topic :" + recordMetadata);
-               e.printStackTrace();
+        	   /*
+        	    * If the broker failed to write the messages, it will return an error. 
+        	    * When the producer receives an error, it may retry sending the message a
+        	    *  few more times before giving up and returning an error
+        	    */
+               logger.debug("Error while producing message to topic :" + recordMetadata,e);
            } else {
-               String message = String.format("sent message to topic:%s partition:%s  offset:%s", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
-               System.out.println(message);
+        	   /*
+        	    * If the messages were successfully written to Kafka, it will return a RecordMetadata 
+        	    * object with the topic, partition, and the offset of the record within the partition. 
+        	    */
+               String message = String.format("OK - Sent message to topic:%s partition:%s  offset:%s", 
+            		   	recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
+               logger.debug(message);
            }
        }
    }
